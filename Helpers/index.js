75349480b3cc3e168f0a5158fa82934b1c7f55a6
SaiSrinivas-10 <saikeesari2000@gmail.com>
2021-07-17 18:50:34 +0530

    json update

--- Helpers/index.js
+++ Helpers/index.js
@@ -1,6 +1,14 @@
 const moviedata = require("../model/videodata");
 const FormData = require("form-data");
 const fs = require("fs");
+const path = require("path");
+
+function base64_encode(file) {
+  // read binary data
+  var bitmap = fs.readFileSync(file);
+  // convert binary data to base64 encoded string
+  return new Buffer.from(bitmap).toString("base64");
+}
 
 const uploadMovie = (req, res) => {
   var tpath = req.files.thumbnail[0].path;
@@ -32,12 +40,14 @@ const getList = (req, res) => {
     .then((movies) => {
       var data = [];
       movies.map((movie) => {
-        var formData = new FormData();
-        formData.append("name", movie.MovieName);
-        formData.append("year", movie.YearOfRelease);
-        formData.append("language", movie.Language);
-        formData.append("thumbnail", fs.createReadStream(`./uploads${movie.ThumbNailPath}`));
-        data.push(formData);
+        var picture = base64_encode(`./uploads${movie.ThumbNailPath}`);
+        var temp = {
+          name: movie.MovieName,
+          year: movie.YearOfRelease,
+          language: movie.Language,
+          thumbnail: picture,
+        };
+        data.push(temp);
       });
       res.send(data);
     })
